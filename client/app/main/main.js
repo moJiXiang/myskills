1445fd38b1b16de2e0edd1864896e21e3ebc2888
moJiXiang <mojixiang1102@gmail.com>
2015-01-14 17:35:10 +0800

    first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('myskillsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
